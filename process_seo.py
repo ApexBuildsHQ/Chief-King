31087687fa30838dd5c7e2c182151a55f3ce3975
Mohamed Mohsen Abdelhady <255518276+ApexBuildsHQ@users.noreply.github.com>
2026-09-26 17:39:17 +0300

    Update input and output directory paths for SEO

--- process_seo.py
+++ process_seo.py
@@ -97,8 +97,8 @@ def main():
 
     chunk_id = args.chunk
     
-    input_file = f"output_recipes_2/recipes_chunk_{chunk_id}.json"
-    output_dir = "output_recipes_seo"
+    input_file = f"output_recipes_seo_2/recipes_chunk_{chunk_id}.json"
+    output_dir = "output_recipes_seo_2"
     output_file = os.path.join(output_dir, f"recipes_chunk_{chunk_id}.json")
 
     os.makedirs(output_dir, exist_ok=True)
